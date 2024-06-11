e75cc2e3f9a66c32d3bd3b64e3a4ed6b1c4ec040
Shubham <ss9971768704@gmail.com>
2024-06-11 10:59:13 +0530

    rd

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -14,9 +14,9 @@ export const GET = async()=>{
         if (!session || !session.user || !session.user._id) {
           return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
-        console.log(session.user);
-        const viewCart  = await Cart.findOne({user: session.user._id}).populate("product")
-        console.log(viewCart);
+        
+        const viewCart  = await Cart.find({user: session.user._id}).populate("product")
+        
         return NextResponse.json({viewCart, message: "Product added in cart successfully" }, { status: 200 });
     } catch (error) {
         console.error("Error getting Cart product:", error);
