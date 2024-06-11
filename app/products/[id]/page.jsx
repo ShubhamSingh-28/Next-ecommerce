f33b24cd547d4daeecde76005447c925ad8bb477
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-06-11 17:54:34 +0530

    Update page.jsx

--- app/products/[id]/page.jsx
+++ app/products/[id]/page.jsx
@@ -80,9 +80,9 @@ const fetchData = async () => {
                     <img className="w-full h-full object-cover" src={data.images?.[0].url} alt="Product Image"/>
                 </div>
                 <div className="flex -mx-2 mb-4">
-                    <Link href={"/cart"} className="w-1/2 px-2">
+                    <div className="w-1/2 px-2">
                         <button className="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700"  onClick={()=>handleClick()}>Add to Cart</button>
-                    </Link>
+</div>
                     <Link href={"/"} className="w-1/2 px-2">
                         <button className="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Go to Shopping</button>
                     </Link>
