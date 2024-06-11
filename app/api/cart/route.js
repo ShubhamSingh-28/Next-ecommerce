e62b4f2b13faab1b6bc7bf918936b28073060f70
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2024-06-12 00:55:36 +0530

    Update route.js

--- app/api/cart/route.js
+++ app/api/cart/route.js
@@ -5,7 +5,7 @@ import { authOptions } from "../auth/[...nextauth]/route";
 import { NextResponse } from "next/server";
 
 
-
+export const dynamic = "force-dynamic";
 
 export const GET = async()=>{
     try {
@@ -15,8 +15,12 @@ export const GET = async()=>{
           return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
         }
         
-        const viewCart = await Cart.find({ user: session.user._id }).populate("product").exec()
-        return NextResponse.json({viewCart, message: " cart getting successfully" }, { status: 200 });
+        const viewCart = await Cart.find({ user: session.user._id }).populate("product")
+        if(viewCart){
+            return NextResponse.json({viewCart, message: " cart getting successfully" }, { status: 200 });
+    }
+        return NextResponse.json({ message: " please try again" }, { status: 204 });
+    
     } catch (error) {
         console.error("Error getting Cart product:", error);
     return NextResponse.json({ error: "Internal server error" }, { status: 500 });
