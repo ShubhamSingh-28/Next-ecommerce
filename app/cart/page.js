1f778999768f715a826dbfe1baadc038e7b44fc4
Shubham <ss9971768704@gmail.com>
2024-06-11 10:39:56 +0530

    s

--- app/cart/page.js
+++ app/cart/page.js
@@ -54,6 +54,7 @@ function Cart() {
       try {
         const prod = await fetch('/api/cart');
         const data2 = await prod.json();
+        console.log(data2);
         setData(data2.viewCart);
         setLoading(false);
       } catch (error) {
