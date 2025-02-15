da739930aeba15259d979f982d0dfdd6a11ee4c9
Shubham Singh <152617144+ShubhamSingh-28@users.noreply.github.com>
2025-02-15 10:24:26 +0530

    Update Promo.jsx

--- components/Promo.jsx
+++ components/Promo.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Promo() {
   return (
-    <div className="relative overflow-hidden bg-white">
+    <div className="relative lg:py-48 py-9 overflow-hidden shadow-lg ">
       <div className="pb-80 pt-16 sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
         <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
           <div className="sm:max-w-lg">
